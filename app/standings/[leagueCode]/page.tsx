598ef99411737d6eeead5ee3d58ed636e2f329d2
itsobi <obiobialo@aol.com>
2024-07-31 23:53:24 -0500

    remove old code

--- app/standings/[leagueCode]/page.tsx
+++ app/standings/[leagueCode]/page.tsx
@@ -57,41 +57,5 @@ export default async function StandingsPage({
         </TableBody>
       </Table>
     </div>
-    // <div className="flex justify-center items-center w-full">
-    //   <table className="w-full">
-    //     {/* TODO: make this dynamic based on the league code */}
-    //     {/* <caption>2024/25 English Premier League Table</caption> */}
-    //     <thead>
-    //       <tr>
-    //         <th className="text-left">Team</th>
-    //         <th className="text-left">Points</th>
-    //         <th className="text-left">Played</th>
-    //         <th className="text-left">Won</th>
-    //         <th className="text-left">Draw</th>
-    //         <th className="text-left"> Lost</th>
-    //       </tr>
-    //     </thead>
-    //     <tbody>
-    //       {standings.map((standing) => (
-    //         <tr key={standing.team.id} className="border mb-4">
-    //           <td className="flex items-center">
-    //             {standing.position}.{' '}
-    //             <img
-    //               src={standing.team.crest}
-    //               alt={standing.team.name}
-    //               className="w-[30px] h-[30px] mx-2"
-    //             />
-    //             {standing.team.name}
-    //           </td>
-    //           <td>{standing.points}</td>
-    //           <td>{standing.playedGames}</td>
-    //           <td>{standing.won}</td>
-    //           <td>{standing.draw}</td>
-    //           <td>{standing.lost}</td>
-    //         </tr>
-    //       ))}
-    //     </tbody>
-    //   </table>
-    // </div>
   );
 }
