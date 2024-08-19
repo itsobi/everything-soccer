98c615bd9693bf13197eeb452736f0bf583a7e75
itsobi <obiobialo@aol.com>
2024-08-18 22:00:53 -0500

    handle undefined coordinates in server component

--- app/[leagueCode]/team/[teamId]/location/[id]/page.tsx
+++ app/[leagueCode]/team/[teamId]/location/[id]/page.tsx
@@ -1,7 +1,9 @@
 import GoogleMap from '@/components/GoogleMap';
 import TeamBreadcrumbMenu from '@/components/TeamBreadcrumbMenu';
-import { getCoordinates } from '@/lib/getCoordinates';
+import { Button } from '@/components/ui/button';
 import { getTeamLocationInfo } from '@/lib/getTeamLocationInfo';
+import { Undo2 } from 'lucide-react';
+import Link from 'next/link';
 
 export default async function MapPage({
   params,
@@ -15,13 +17,29 @@ export default async function MapPage({
   return (
     <div className="w-full min-h-screen">
       <TeamBreadcrumbMenu leagueCode={params.leagueCode} teamId={params.id} />
-      <div className="mt-6">
-        <GoogleMap
-          teamInfo={teamInfo}
-          latitude={latitude}
-          longitude={longitude}
-        />
-      </div>
+      {latitude && longitude ? (
+        <div className="mt-6">
+          <GoogleMap
+            teamInfo={teamInfo}
+            latitude={latitude}
+            longitude={longitude}
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center mt-7 lg:text-xl text-center space-y-8">
+          <h1>
+            Sorry, we could not get the location coordinates for {teamInfo.name}
+          </h1>
+          <Button
+            asChild
+            className="animate-bounce border rounded-full hover:bg-[#e52534] hover:text-white"
+          >
+            <Link href={`/${params.leagueCode}/team/${params.id}`}>
+              <Undo2 />
+            </Link>
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -3,9 +3,6 @@
 import { TeamLocationInfo } from '@/lib/getTeamLocationInfo';
 import { AdvancedMarker, InfoWindow, Map } from '@vis.gl/react-google-maps';
 import { useState } from 'react';
-import { Button } from './ui/button';
-import { Undo2 } from 'lucide-react';
-import { useRouter } from 'next/navigation';
 
 type Props = {
   teamInfo: TeamLocationInfo;
@@ -15,54 +12,37 @@ type Props = {
 
 export default function GoogleMap({ teamInfo, latitude, longitude }: Props) {
   const [open, setOpen] = useState(false);
-  const router = useRouter();
 
-  if (!latitude && !longitude) {
-    return (
-      <div className="flex flex-col justify-center items-center mt-7 lg:text-xl space-y-8">
-        <h1>
-          Sorry, we could not get the location coordinates for {teamInfo.name}
-        </h1>
-        <Button
-          onClick={() => router.back()}
-          className="animate-bounce border rounded-full hover:bg-[#e52534] hover:text-white"
-        >
-          <Undo2 />
-        </Button>
-      </div>
-    );
-  } else {
-    return (
-      <Map
-        style={{ height: '100vh' }}
-        defaultZoom={10}
-        center={{ lat: Number(latitude), lng: Number(longitude) }}
-        mapId={process.env.NEXT_PUBLIC_MAP_ID}
-      >
-        <AdvancedMarker
+  return (
+    <Map
+      style={{ height: '100vh' }}
+      defaultZoom={10}
+      center={{ lat: Number(latitude), lng: Number(longitude) }}
+      mapId={process.env.NEXT_PUBLIC_MAP_ID}
+    >
+      <AdvancedMarker
+        position={{ lat: Number(latitude), lng: Number(longitude) }}
+        onClick={() => setOpen(!open)}
+      />
+
+      {open && (
+        <InfoWindow
           position={{ lat: Number(latitude), lng: Number(longitude) }}
-          onClick={() => setOpen(!open)}
-        />
+        >
+          <div className="p-2 bg-white shadow-md rounded flex flex-col space-y-2">
+            <img
+              src={teamInfo.crest}
+              alt={teamInfo.name}
+              className="w-[35px] h-[35px]"
+            />
 
-        {open && (
-          <InfoWindow
-            position={{ lat: Number(latitude), lng: Number(longitude) }}
-          >
-            <div className="p-2 bg-white shadow-md rounded flex flex-col space-y-2">
-              <img
-                src={teamInfo.crest}
-                alt={teamInfo.name}
-                className="w-[35px] h-[35px]"
-              />
+            <h4 className="font-semibold">{teamInfo.name}</h4>
 
-              <h4 className="font-semibold">{teamInfo.name}</h4>
-
-              <p>{teamInfo.venue}</p>
-              <p>{teamInfo.address}</p>
-            </div>
-          </InfoWindow>
-        )}
-      </Map>
-    );
-  }
+            <p>{teamInfo.venue}</p>
+            <p>{teamInfo.address}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </Map>
+  );
 }
